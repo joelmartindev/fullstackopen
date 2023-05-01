b1ba1f62c89c2949681ec404db9f78ff0cfa094a
joelmartindev <joel.martin@metropolia.fi>
2023-05-01 16:56:02 +0300

    Ex. 4.5: Favorite blogs

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -64,3 +64,85 @@ describe('total likes', () => {
     expect(result).toBe(24)
   })
 })
+
+describe('blog with most likes', () => {
+
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+  const blogsWithEqualLikes = [
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('when there is only one with the greatest value, return it', () => {
+    const result = listHelper.favoriteBlog(blogs)
+
+    expect(result).toEqual(
+      {
+        title: "React patterns",
+      author: "Michael Chan",
+      likes: 12
+      }
+    )
+  })
+  
+  test('when there are blogs with equal likes, return one of them', () => {
+    const result = listHelper.favoriteBlog(blogsWithEqualLikes)
+
+    expect(result).toEqual(
+      {
+        title: "React patterns",
+      author: "Michael Chan",
+      likes: 12
+      }
+    )
+  })
+
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,10 +6,20 @@ const totalLikes = (blogs) => {
     
     return blogs.length === 0
         ? 0
-        : blogs.reduce((total, currentValue, currentIndex) => total + blogs[currentIndex].likes, 0)
+        : blogs.reduce((total, currentBlog) => total + currentBlog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    // returns last blog with greatest value of likes
+    return blogs.reduce((favorite, current) => {
+        return favorite.likes > current.likes
+        ? favorite
+        : {title: current.title, author: current.author, likes: current.likes}
+    }, {likes: -1})
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
