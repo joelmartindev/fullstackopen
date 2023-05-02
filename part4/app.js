6f222b3d7ad36108bd82c6996784bfca5aff0560
joelmartindev <joel.martin@metropolia.fi>
2023-05-02 17:05:30 +0300

    Current progress on Ex. 4.15

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const { MONGODB_URI } = require('./utils/config')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(MONGODB_URI)
@@ -12,6 +13,7 @@ mongoose.connect(MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
